89fb5f5e0fee952c7d85aef9e2dc8ae11880d6f3
Leslie <lesliek.x1999@gmail.com>
2024-12-14 20:57:34 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,16 @@ const path = window.location.pathname
 console.log(path)
 switch(path){
     case '/index.html':
+        console.log('primera')
         primerasV();
         primerosA();
         break;
     case '/propiedades_venta.html':
+        console.log('segunda')
         ventas();
     break;
     case '/propiedades_alquiler.html':
+        console.log('tercera')
         alquiler();
     break;
 
